23f4fcdcc67de7a7f65fee219c5edea34b312374
Ayush Gupta <12211028a@gmail.com>
2025-12-20 13:11:37 +0530

    changed type to module

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 export async function connectDatabase() {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`MongoDB connected successfully: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed", error);
